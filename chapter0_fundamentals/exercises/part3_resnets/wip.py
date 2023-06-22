d6db573b97f971570b7c69cc207d019e02634835
Zane Dufour <zane@znd4.me>
2023-06-22 15:10:31 -0400

    finish resnets part 2

--- chapter0_fundamentals/exercises/part3_resnets/wip.py
+++ chapter0_fundamentals/exercises/part3_resnets/wip.py
@@ -1,4 +1,6 @@
 import os
+import json
+from PIL import Image
 import einops
 import sys
 import textwrap
@@ -6,7 +8,7 @@ from pathlib import Path
 from typing import Tuple, Callable
 
 from pydantic.dataclasses import dataclass
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, models
 
 import pytorch_lightning as pl
 import torch as t
@@ -27,6 +29,7 @@ if str(exercises_dir) not in sys.path:
 os.chdir(section_dir)
 
 import part3_resnets.tests as tests
+from part3_resnets.utils import print_param_count
 from part2_cnns.solutions import Linear, Conv2d, Flatten, ReLU, MaxPool2d
 
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
@@ -242,7 +245,194 @@ class BatchNorm2d(nn.Module):
         )
 
 
+class AveragePool(nn.Module):
+    """
+    x: shape (batch, channels, height, width)
+    Return: shape (batch, channels)
+    """
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        return t.mean(x, dim=(2, 3))
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_feats: int, out_feats: int, first_stride=1):
+        """
+        A single residual block with optional downsampling.
+
+        For compatibility with the pretrained model, declare the left side branch first using a `Sequential`.
+
+        If first_stride is > 1, this means the optional (conv + bn) should be present on the right branch. Declare it second using another `Sequential`.
+        """
+        super().__init__()
+        self.left = nn.Sequential(
+            Conv2d(in_feats, out_feats, kernel_size=3, stride=first_stride, padding=1),
+            BatchNorm2d(out_feats),
+            ReLU(),
+            Conv2d(out_feats, out_feats, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(out_feats),
+        )
+        self.right = (
+            nn.Identity()
+            if first_stride == 1
+            else nn.Sequential(
+                Conv2d(
+                    in_feats, out_feats, kernel_size=1, stride=first_stride, padding=0
+                ),
+                BatchNorm2d(out_feats),
+            )
+        )
+        self.relu = ReLU()
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        """
+        Compute the forward pass.
+
+        x: shape (batch, in_feats, height, width)
+
+        Return: shape (batch, out_feats, height / stride, width / stride)
+
+        If no downsampling block is present, the addition should just add the left branch's output to the input.
+        """
+        return self.relu(self.left(x) + self.right(x))
+
+
+class BlockGroup(nn.Module):
+    def __init__(self, n_blocks: int, in_feats: int, out_feats: int, first_stride=1):
+        """An n_blocks-long sequence of ResidualBlock where only the first block uses the provided stride."""
+        super().__init__()
+        self.blocks = nn.Sequential(
+            ResidualBlock(in_feats, out_feats, first_stride),
+            *(ResidualBlock(out_feats, out_feats) for _ in range(n_blocks - 1)),
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        """
+        Compute the forward pass.
+
+        x: shape (batch, in_feats, height, width)
+
+        Return: shape (batch, out_feats, height / first_stride, width / first_stride)
+        """
+        return self.blocks(x)
+
+
+class ResNet34(nn.Module):
+    def __init__(
+        self,
+        n_blocks_per_group=[3, 4, 6, 3],
+        out_features_per_group=[64, 128, 256, 512],
+        first_strides_per_group=[1, 2, 2, 2],
+        n_classes=1000,
+    ):
+        super().__init__()
+        self.blocks = nn.Sequential(
+            Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            BatchNorm2d(64),
+            ReLU(),
+            MaxPool2d(kernel_size=3, stride=2),
+            *(
+                BlockGroup(n_blocks, in_features, out_features, first_stride)
+                for n_blocks, in_features, out_features, first_stride in zip(
+                    n_blocks_per_group,
+                    (64, *out_features_per_group[:-1]),
+                    out_features_per_group,
+                    first_strides_per_group,
+                )
+            ),
+            AveragePool(),
+            Flatten(),
+            Linear(out_features_per_group[-1], n_classes),
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        """
+        x: shape (batch, channels, height, width)
+        Return: shape (batch, n_classes)
+        """
+        return self.blocks(x)
+
+
+def copy_weights(
+    my_resnet: ResNet34, pretrained_resnet: models.resnet.ResNet
+) -> ResNet34:
+    """Copy over the weights of `pretrained_resnet` to your resnet."""
+
+    # Get the state dictionaries for each model, check they have the same number of parameters & buffers
+    mydict = my_resnet.state_dict()
+    pretraineddict = pretrained_resnet.state_dict()
+    assert len(mydict) == len(pretraineddict), "Mismatching state dictionaries."
+
+    # Define a dictionary mapping the names of your parameters / buffers to their values in the pretrained model
+    state_dict_to_load = {
+        mykey: pretrainedvalue
+        for (mykey, myvalue), (pretrainedkey, pretrainedvalue) in zip(
+            mydict.items(), pretraineddict.items()
+        )
+    }
+
+    # Load in this dictionary to your model
+    my_resnet.load_state_dict(state_dict_to_load)
+
+    return my_resnet
+
+
+IMAGE_SIZE = 224
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+IMAGENET_TRANSFORM = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+    ]
+)
+
+
+def prepare_data(images: list[Image.Image]) -> t.Tensor:
+    """
+    Return: shape (batch=len(images), num_channels=3, height=224, width=224)
+    """
+    return t.stack(list(map(IMAGENET_TRANSFORM, images)), dim=0)
+
+
+def predict(model, images):
+    logits: t.Tensor = model(images)
+    return logits.argmax(dim=1)
+
+
 def main():
-    tests.test_batchnorm2d_module(BatchNorm2d)
-    tests.test_batchnorm2d_forward(BatchNorm2d)
-    tests.test_batchnorm2d_running_mean(BatchNorm2d)
+    my_resnet = ResNet34()
+    pretrained_resnet = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1)
+    my_resnet = copy_weights(my_resnet, pretrained_resnet)
+
+    IMAGE_FILENAMES = [
+        "chimpanzee.jpg",
+        "golden_retriever.jpg",
+        "platypus.jpg",
+        "frogs.jpg",
+        "fireworks.jpg",
+        "astronaut.jpg",
+        "iguana.jpg",
+        "volcano.jpg",
+        "goofy.jpg",
+        "dragonfly.jpg",
+    ]
+
+    IMAGE_FOLDER = section_dir / "resnet_inputs"
+
+    images = [Image.open(IMAGE_FOLDER / filename) for filename in IMAGE_FILENAMES]
+    prepared_images = prepare_data(images)
+    assert prepared_images.shape == (len(images), 3, IMAGE_SIZE, IMAGE_SIZE)
+
+    with open(section_dir / "imagenet_labels.json") as f:
+        imagenet_labels = list(json.load(f).values())
+
+    my_predictions = predict(my_resnet, prepared_images)
+    pretrained_predictions = predict(pretrained_resnet, prepared_images)
+    assert all(my_predictions == pretrained_predictions)
+
+
+if MAIN:
+    main()
